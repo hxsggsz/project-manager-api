cc80aba1f66b5e32eea4b7ce636a695c1fb89fa2
Victor Hugo <vt.hugo.2021@gmail.com>
2023-07-24 17:11:17 -0300

    add more tests for login use case

--- src/app/use-cases/login-user.spec.ts
+++ src/app/use-cases/login-user.spec.ts
@@ -4,6 +4,7 @@ import { makeUser } from '../../../test/factories/user-factory';
 import { UserInfoPassword } from '../entities/user/user-info-password';
 import * as bcrypt from 'bcrypt';
 import { InMemoryJwtService } from '../../../test/repositories/in-memory-jwt-service';
+import { UserNotFound } from './errors/user-not-found';
 
 describe('login user UseCase', () => {
   it('should be able to login the user', async () => {
@@ -28,5 +29,46 @@ describe('login user UseCase', () => {
 
     expect(access_token).toBeDefined();
     expect(access_token).toBe('logged');
+  });
+
+  it('should not login with wrong email', async () => {
+    const inMemoryJwtService = new InMemoryJwtService();
+    const userMemoryRepository = new InMemoryUserRepository();
+    const loginUserRepository = new LoginUser(
+      userMemoryRepository,
+      inMemoryJwtService,
+    );
+
+    expect(
+      async () =>
+        await loginUserRepository.execute({
+          email: 'emailnotdefined@gmail.com',
+          password: 'teste!A1',
+        }),
+    ).rejects.toThrow(UserNotFound);
+  });
+
+  it('should not login with wrong password', async () => {
+    const inMemoryJwtService = new InMemoryJwtService();
+    const userMemoryRepository = new InMemoryUserRepository();
+    const loginUserRepository = new LoginUser(
+      userMemoryRepository,
+      inMemoryJwtService,
+    );
+
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash('teste!A1', salt);
+    const userCrypto = makeUser({
+      password: new UserInfoPassword(hashPassword),
+    });
+    await userMemoryRepository.create(userCrypto);
+
+    expect(
+      async () =>
+        await loginUserRepository.execute({
+          email: userMemoryRepository.userMemory[0].email.value,
+          password: 'passwordWrong!123',
+        }),
+    ).rejects.toThrow(UserNotFound);
   });
 });
