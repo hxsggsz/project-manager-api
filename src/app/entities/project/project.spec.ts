26a15f52d32bddfe0b07ff2be5522968db8c5adf
Victor Hugo <vt.hugo.2021@gmail.com>
2023-07-25 23:56:02 -0300

    add more tests for project and user entities

--- src/app/entities/project/project.spec.ts
+++ src/app/entities/project/project.spec.ts
@@ -14,7 +14,30 @@ describe('projects entity', () => {
 
     expect(project).toBeTruthy();
     expect(project.name.value).toEqual('testName');
+    expect(project.isPublic).toBeTruthy();
     expect(project.ownerId).toEqual('idTest');
     expect(project.createdAt).toEqual(date);
+  });
+
+  it('should create a new entity and after update the entity propieties', () => {
+    const date = jest.fn().mockReturnValue(new Date());
+
+    const project = new Project({
+      name: new ProjectName('testName'),
+      isPublic: true,
+      ownerId: 'idTest',
+      createdAt: date as unknown as Date,
+    });
+
+    expect(project).toBeTruthy();
+    expect(project.name.value).toEqual('testName');
+    expect(project.isPublic).toBeTruthy();
+    expect(project.ownerId).toEqual('idTest');
+    expect(project.createdAt).toEqual(date);
+
+    project.updateProject('new name', false);
+
+    expect(project.name.value).toEqual('new name');
+    expect(project.isPublic).toBeFalsy();
   });
 });
--- src/app/entities/user/user.spec.ts
+++ src/app/entities/user/user.spec.ts
@@ -6,7 +6,7 @@ import { UserInfoProfilePhoto } from './user-info-profile-photo';
 import { UserInfoUsername } from './user-info-username';
 
 describe('user entity', () => {
-  it('should create the user correctly', () => {
+  it('should create the user entity correctly', () => {
     const date = jest.fn().mockReturnValue(new Date());
 
     const user = new User({
@@ -25,5 +25,36 @@ describe('user entity', () => {
     expect(user.password.value).toEqual('teste!A1');
     expect(user.profilePhoto.value).toEqual('https://image.com');
     expect(user.createdAt).toEqual(date);
+  });
+
+  it('should create the user entity and update it correctly', () => {
+    const date = jest.fn().mockReturnValue(new Date());
+
+    const user = new User({
+      name: new UserInfoName('test'),
+      username: new UserInfoUsername('username'),
+      email: new UserInfoEmail('test@gmail.com'),
+      password: new UserInfoPassword('teste!A1'),
+      profilePhoto: new UserInfoProfilePhoto('https://image.com'),
+      createdAt: date as unknown as Date,
+    });
+
+    expect(user).toBeTruthy();
+    expect(user.name.value).toEqual('test');
+    expect(user.username.value).toEqual('username');
+    expect(user.email.value).toEqual('test@gmail.com');
+    expect(user.password.value).toEqual('teste!A1');
+    expect(user.profilePhoto.value).toEqual('https://image.com');
+    expect(user.createdAt).toEqual(date);
+
+    user.updateUser(
+      new UserInfoUsername('new username'),
+      new UserInfoName('new name'),
+      new UserInfoProfilePhoto('https://newPhoto.com'),
+    );
+
+    expect(user.name.value).toEqual('new name');
+    expect(user.username.value).toEqual('new username');
+    expect(user.profilePhoto.value).toEqual('https://newPhoto.com');
   });
 });
